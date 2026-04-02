02d599ecef5fcd3b8f26e45d4d410da27e3f28b6
Кирилл <84809375+SKD115@users.noreply.github.com>
2026-04-02 15:22:28 +0300

    Update Case2.cs
    Смолякин Кирилл ИДБ-24-18

--- ConsoleApp2/ocp/Case2.cs
+++ ConsoleApp2/ocp/Case2.cs
@@ -1,110 +1,89 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2.ocp
 {
-    class Case2
+    class Case1
     {
-        class Circle
+        interface ICoolGuy
         {
-            public double Radius;
-            public Circle(double radius)
-            {
-                Radius = radius;
-            }
-            public double GetArea()
-            {
-                return Math.PI * Radius * Radius;
-            }
+            void CallCoolGuy();
         }
-        class Rectangle
+        // ЧТО: Добавили интерфейс расширения отрисовки пользователя
+        // ЗАЧЕМ: Чтобы новые сценарии рисования добавлять через новые классы, не меняя основной метод User.DrawUser(), соблюдая OCP
+        interface IUserDrawExtension
         {
-            public double Width;
-            public double Height;
-            public Rectangle(double width, double height)
-            {
-                Width = width;
-                Height = height;
-            }
-            public double GetArea()
-            {
-                return Width * Height;
-            }
+            bool CanDraw(User user);
+            void Draw(User user);
         }
-        class AreaCalculator
+
+        class User
         {
-            public double Calculate(object shape)
+            private readonly bool _isSelected;
+            private readonly string _image;
+            // ЧТО: Добавили патерн для разных ситуаций
+            // ЗАЧЕМ: Теперь для редких/специальных случаев не нужно изменять User
+            private readonly IEnumerable<IUserDrawExtension> _drawExtensions;
+
+            public User(bool isSelected, string image, IEnumerable<IUserDrawExtension> drawExtensions = null)
             {
-                if (shape is Circle)
+                _isSelected = isSelected;
+                _image = image;
+                _drawExtensions = drawExtensions ?? new List<IUserDrawExtension>();
+            }
+
+            public void DrawUser()
+            {
+                if (_isSelected)
+                    DrawEllipseAroundUser();
+
+                if (_image != null)
+                    DrawImageOfUser();
+                // ЧТО: Убрали проверку "if (this is ICoolGuy)" и заменили проверку патернов
+                // ЗАЧЕМ:Раньше для каждого нового патерна пришлось бы менять DrawUser(). Теперь новые патерны просто подключаются как новые IUserDrawExtension
+                foreach (var extension in _drawExtensions)
                 {
-                    return ((Circle)shape).GetArea();
+                    if (extension.CanDraw(this))
+                        extension.Draw(this);
                 }
-                else if (shape is Rectangle)
-                {
-                    return ((Rectangle)shape).GetArea();
-                }
-                return 0;
             }
+            // ЧТО: Добавили свойства только для чтения.
+            // ЗАЧЕМ: Чтобы патерны могли безопасно получать данные пользователя,  не пытаясь взять данные из приватных полей напрямую
+            public bool IsSelected => _isSelected;
+            public string Image => _image;
+
+            void DrawEllipseAroundUser() { }
+            void DrawImageOfUser() { }
+            // ЧТО: Сделали метод internal, чтобы патерн мог вызвать нужную отрисовку
+            // ЗАЧЕМ: Логика рисования очков остаётся внутри User, но решение КОГДА рисовать вынесено наружу
+            internal void DrawCoolGuyGlasses() { }
         }
-        class Program
+        // ЧТО: Специальный случай "крутой парень" вынесен в отдельный патерн
+        // ЗАЧЕМ: Теперь эта логика расширяет систему без изменения класса User
+        class CoolGuyDrawExtension : IUserDrawExtension
         {
-            static void Main()
+            public bool CanDraw(User user)
             {
-                Circle c = new Circle(5);
-                Rectangle r = new Rectangle(4, 6);
-                AreaCalculator calculator = new AreaCalculator();
-                Console.WriteLine("Circle area: " + calculator.Calculate(c));
-                Console.WriteLine("Rectangle area: " + calculator.Calculate(r));
+                return user is ICoolGuy;
+            }
+
+            public void Draw(User user)
+            {
+                user.DrawCoolGuyGlasses();
             }
         }
 
+        class CoolUser : User, ICoolGuy
+        {
+            public CoolUser(bool isSelected, string image, IEnumerable<IUserDrawExtension> drawExtensions = null)
+                : base(isSelected, image, drawExtensions)
+            {
+            }
 
-        //class Figure
-        //{
-        //    public virtual double GetArea()
-        //    {
-        //        return 0;
-        //    }
-        //}
-
-        //class Circle : Figure
-        //{
-        //    public double Radius { get; set; }
-        //    public Circle(double radius)
-        //    {
-        //        Radius = radius;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Math.PI * Radius * Radius;
-        //    }
-        //}
-        //class Rectangle : Figure
-        //{
-        //    public double Width { get; set; }
-        //    public double Height { get; set; }
-        //    public Rectangle(double width, double height)
-        //    {
-        //        Width = width;
-        //        Height = height;
-        //    }
-        //    public override double GetArea()
-        //    {
-        //        return Width * Height;
-        //    }
-        //}
-        //class Application
-        //{
-        //    static void Main()
-        //    {
-        //        var c = new Circle(5);
-        //        var r = new Rectangle(4, 6);
-        //        Console.WriteLine("Circle area: " + c.GetArea());
-        //        Console.WriteLine("Rectangle area: " + r.GetArea());
-        //    }
-        //}
+            public void CallCoolGuy()
+            {
+                // бла бла логика
+            }
+        }
     }
 }
